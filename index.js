2260ac364d845eefdfd8c766447fbead086d9767
powellstreet <codestates.jc@gmail.com>
2020-04-08 15:28:15 +0900

    Server Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
