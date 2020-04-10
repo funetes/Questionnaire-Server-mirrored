ef9859807ffa66703bbc3b4e9566eb6d1423a1fb
powellstreet <codestates.jc@gmail.com>
2020-04-10 14:12:14 +0900

    :rocket: Paused for Socket.io routing

--- Router/audience.js
+++ Router/audience.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Event } = require('../models');
+const { io } = require('../index')
 
 const router = express.Router();
 
@@ -15,10 +16,25 @@ router.post('/join', (req, res) => {
         res.status(409).json({ result: 'fail' });
       } else {
         // data.id 를 이용해서 리다이렉션 + 해당 이벤트 질문들 쏴주기
-        res.status(200).json(data);
+        res.status(200).json({ eventId: data.id });
+
+        const namespace1 = io.of('/:eventid');
+        namespace1.on('connection', (socket) => {
+          namespace1.emit('news', { hello: 'Someone connected at namespace1' });
+        });
       }
     });
 });
+
+
+/*
+// NameSpace 1번
+const namespace1 = io.of('/:eventid');
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace1.on('connection', (socket) => {
+  namespace1.emit('news', { hello: 'Someone connected at namespace1' });
+});
+*/
 
 
 module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const cors = require('cors');
 
 const port = process.env.PORT;
@@ -17,7 +18,7 @@ const presentor = require('./Router/presentor');
 const event = require('./Router/event');
 
 const verification = function (req, res, next) {
-  const token = req.headers.authorization.slice(7);
+  const token = req.headers.authorization ? req.headers.authorization.slice(7) : null;
 
   if (!token) {
     next();
@@ -40,7 +41,6 @@ const verification = function (req, res, next) {
   }
 };
 
-
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors({
@@ -49,11 +49,46 @@ app.use(cors({
 
 app.use(verification);
 
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
 // 라우팅
 app.use('/user', user);
 app.use('/audience', audience);
 app.use('/presentor', presentor);
 app.use('/event', event);
+
+
+// SocketIO
+// NameSpace 1번
+const namespace1 = io.of('/:eventid');
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace1.on('connection', (socket) => {
+  namespace1.emit('news', { hello: 'Someone connected at namespace1' });
+});
+
+
+
+
+/*
+ 식별:cookie?
+
+ 글쓰기
+ - client에서 post_create 이벤트 emit
+ - 서버에서 post_create 이벤트 on
+ - 받은 데이터 바탕으로 post_update 이벤트 emit
+ - client에서는 post_updated 이벤트 on
+
+  좋아요
+ - client에서 like_create 이벤트 emit
+ - 서버에서 like_create 이벤트 on
+ - 받은 데이터 바탕으로 like_update 이벤트 emit
+ - client에서는 like_updated 이벤트 on
+
+
+});
+*/
+
 
 http.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
